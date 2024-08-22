e42339a4905b0da68362ebbdf0ffa74a935fd0cb
Vijay Kumar <saroyav3@gmail.com>
2024-08-22 21:01:21 +1200

    gradient color added to app

--- vs_code/demo_project/demo_application/lib/main.dart
+++ vs_code/demo_project/demo_application/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:demo_application/pages/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Home(),
     );
   }
 }
--- vs_code/demo_project/demo_application/lib/pages/home.dart
+++ vs_code/demo_project/demo_application/lib/pages/home.dart
@@ -11,12 +11,21 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(child: Column(
+      body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(gradient: LinearGradient(colors:[color(0xFF232FDA2)])),
-        child: Column(children: [])
-      )),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF232FDA), // Correct usage of Color
+              Color(0xFF23DAF2)  // Added a second color for the gradient
+            ]
+          ),
+        ),
+        child: Column(
+          children: [], // You can add children here
+        ),
+      ),
     );
   }
 }
